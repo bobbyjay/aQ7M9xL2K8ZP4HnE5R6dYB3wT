657c7bc453f83b36322d76bc0e00a2ad5346ae33
bobbyjay <jaymuhammed401@gmail.com>
2026-07-08 03:11:57 -0700

    added react-router.config.js

---
+++ react-router.config.js
@@ -0,0 +1,11 @@
+export default {
+  ssr: true,
+
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
+};
