65a5801b0d3cc39161b2752d7625f1f6764583f8
bobbyjay <jaymuhammed401@gmail.com>
2026-07-08 02:59:49 -0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { reactRouter } from '@react-router/dev/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default defineConfig({
-  plugins: [react(), reactRouter()],
-})
+  plugins: [
+    react(),
+    reactRouter({
+      presets: [vercelPreset()],
+    }),
+  ],
+});
