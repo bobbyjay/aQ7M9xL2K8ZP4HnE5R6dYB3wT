41892a9fab747bd2a45a35a505cdc89cb9f02ff2
bobbyjay <jaymuhammed401@gmail.com>
2026-07-10 02:39:09 -0700

    added useEffect to index route

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,6 @@
 import Home from "../../src/pages/Home";
 import Seo from "../../src/components/Seo";
+import { useEffect } from "react";
 // import "../../src/js/es34model.js?url";
 
 export const handle = {
@@ -27,7 +28,7 @@ export default function IndexRoute() {
       document.body.removeChild(script); // Clean up on unmount
     };
   }, []);
-  
+
   return (
     <>
       <Seo title="ClutchDen | Data-Driven Sports Intelligence & Winning Strategies" description="Explore ClutchDen for professional sports analysis, data-driven insights, and winning strategies." />
